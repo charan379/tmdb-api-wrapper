ff7d541c5868f51d7f106cfa173719ac5ce3ef1f
charan379 <37008444+charan379@users.noreply.github.com>
2023-02-07 18:08:21 +0530

    initialize

---
+++ app.js
@@ -0,0 +1,43 @@
+import createError from 'http-errors';
+import express, { json, urlencoded } from 'express';
+import { join } from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import { middleware } from 'stylus';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+const app = express();
+
+// view engine setup
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(middleware(join(__dirname, 'public')));
+app.use(express.static(join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
