5de692c31f4a79d495993eb6d6ce774f68769beb
charan379 <37008444+charan379@users.noreply.github.com>
2023-02-08 16:30:00 +0530

    minor tweaks

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-var router = Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-export default router;
+module.exports = router;
