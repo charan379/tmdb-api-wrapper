5a6540045086e6fc632fdc054e8ce46d855eec2b
charan379 <37008444+charan379@users.noreply.github.com>
2023-02-08 16:31:36 +0530

    tmdb config added

---
+++ utils/TmdbConfig.js
@@ -0,0 +1,9 @@
+const TmdbConfig = {
+  tmdbApiKey: process.env.APP_TMDB_API,
+  tmdbApiUrl: process.env.APP_TMDB_API_URL,
+  tmdbImagesUrl: process.env.APP_TMDB_API_IMAGES_URL,
+  tmdbLanguage: process.env.APP_TMDB_API_LANGUAGE,
+  tmdbRegion: process.env.APP_TMDB_API_REGION,
+};
+
+module.exports = TmdbConfig;
