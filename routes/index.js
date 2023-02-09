9f86e83f30af979d67f3781d152f9cbdd5193d7e
charan379 <37008444+charan379@users.noreply.github.com>
2023-02-09 15:47:33 +0530

    name changed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tmdb API Wraapper by @charan379' });
 });
 
 module.exports = router;
