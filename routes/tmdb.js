56ba0bf034d3fb1574a554f9ebd209057f77a6ca
charan379 <37008444+charan379@users.noreply.github.com>
2023-02-09 15:46:27 +0530

    swagger documention added

--- routes/tmdb.js
+++ routes/tmdb.js
@@ -8,19 +8,92 @@ const router = express.Router();
 
 /* GET tmdb home. */
 router.get("/", function (req, res, next) {
-  res.send("api working");
+  res.render("tmdb", { title: "Tmdb-api", message: "API Up And Running" });
 });
 
+/**
+ *     requestBody:
+ *      required: false
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              notesID:
+ *                description: Id of the new notes
+ *                type: Number
+ *                example: 1234
+ *              name:
+ *                description: Name of the person
+ *                type: String
+ *                example: Alex
+ */
+
+
+
+/**
+ * @swagger
+ * /tmdb/config:
+ *  get:
+ *    summary: API to fetch configuration of tmdb
+ *    description: Retirve a list of configuration variables
+ *    responses:
+ *      200:
+ *        description: Success
+ *      400:
+ *       description: No Data Avaialble
+ */
 router.get("/config", function (req, res, next) {
   res.status(200).json({
     ...TmdbConfig,
   });
 });
 
+
+/**
+ * @swagger
+ * /tmdb/search:
+ *  get:
+ *    summary: API to fetch movies or tv shows from tmdb
+ *    description: Retirve a list of movies or tv shows from tmdb
+ *    parameters:
+ *    - in: query
+ *      name: query
+ *      schema:
+ *        type: String
+ *      description: Movie or TV Name
+ *      default : fight club
+ *    - in: query
+ *      name: type
+ *      schema:
+ *        type: String
+ *        enum:
+ *          - movie
+ *          - tv
+ *      description: Movie or TV
+ *      default : movie
+ *    - in: query
+ *      name: year
+ *      schema:
+ *        type: Number
+ *      description: Release year of movie or tv  show
+ *      default : 1999
+ *    - in: query
+ *      name: pageNo
+ *      schema:
+ *        type: Number
+ *      description: page number
+ *      default : 1
+ *    responses:
+ *      200:
+ *        description: Success
+ *      404:
+ *       description: No Data Avaialble
+ */
 /* GET tmdb search */
 router.get("/search", function (req, res, next) {
   try {
-    searchTmdb({...req.query})
+    searchTmdb({ ...req.query })
       .then((response) => {
         res.status(200).json(response);
       })
@@ -42,70 +115,70 @@ router.get("/search", function (req, res, next) {
 
 /* GET tmdb movie details */
 router.get("/movie/:tmdb_id", function (req, res, next) {
-    try {
-        getTmdbMovie(req.params.tmdb_id)
-        .then((response) => {
-          res.status(200).json(response);
-        })
-        .catch((error) => {
-          res.status(404).json({
-            code: 404,
-            errorMessage: error.message,
-            stauts: "Request Failed at Controller",
-          });
+  try {
+    getTmdbMovie(req.params.tmdb_id)
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((error) => {
+        res.status(404).json({
+          code: 404,
+          errorMessage: error.message,
+          stauts: "Request Failed at Controller",
         });
-    } catch (error) {
-      res.status(500).json({
-        code: 500,
-        errorMessage: "Internal Server Error 500",
-        status: "Request Faild at Router",
       });
-    }
-  });
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      errorMessage: "Internal Server Error 500",
+      status: "Request Faild at Router",
+    });
+  }
+});
 
 /* GET tmdb tv details */
 router.get("/tv/:tmdb_id", function (req, res, next) {
-    try {
-        getTmdbTv(req.params.tmdb_id)
-        .then((response) => {
-          res.status(200).json(response);
-        })
-        .catch((error) => {
-          res.status(404).json({
-            code: 404,
-            errorMessage: error.message,
-            stauts: "Request Failed at Controller",
-          });
+  try {
+    getTmdbTv(req.params.tmdb_id)
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((error) => {
+        res.status(404).json({
+          code: 404,
+          errorMessage: error.message,
+          stauts: "Request Failed at Controller",
         });
-    } catch (error) {
-      res.status(500).json({
-        code: 500,
-        errorMessage: "Internal Server Error 500",
-        status: "Request Faild at Router",
       });
-    }
-  });
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      errorMessage: "Internal Server Error 500",
+      status: "Request Faild at Router",
+    });
+  }
+});
 
 /* GET tmdb tv-season details */
 router.get("/tv/:tmdb_tv_id/season/:season_number", function (req, res, next) {
-    try {
-        getTmdbTvSeason({...req.params})
-        .then((response) => {
-          res.status(200).json(response);
-        })
-        .catch((error) => {
-          res.status(404).json({
-            code: 404,
-            errorMessage: error.message,
-            stauts: "Request Failed at Controller",
-          });
+  try {
+    getTmdbTvSeason({ ...req.params })
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((error) => {
+        res.status(404).json({
+          code: 404,
+          errorMessage: error.message,
+          stauts: "Request Failed at Controller",
         });
-    } catch (error) {
-      res.status(500).json({
-        code: 500,
-        errorMessage: "Internal Server Error 500",
-        status: "Request Faild at Router",
       });
-    }
-  });
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      errorMessage: "Internal Server Error 500",
+      status: "Request Faild at Router",
+    });
+  }
+});
 module.exports = router;
